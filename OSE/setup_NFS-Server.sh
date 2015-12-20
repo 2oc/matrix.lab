6d14e5e9d301816a0837c692a8eb8c0c607e6948
jradtke-rh <jradtke@redhat.com>
2015-12-20 08:40:55 -0500

    Fixes

--- OSE/setup_NFS-Server.sh
+++ OSE/setup_NFS-Server.sh
@@ -14,30 +14,30 @@ for VOLUME in pv{1..10}
 do
   lvcreate -L${LVSIZE} -nlv_${VOLUME} $VG
   mkfs.xfs -f /dev/mapper/${VG}-lv_${VOLUME}
-  mkdir -p /export/nfs/pvs/${VOLUME}
-  echo "/dev/mapper/${VG}-lv_${VOLUME} /export/nfs/pvs/${VOLUME} xfs defaults 0 0" >> /etc/fstab
-  echo "/export/nfs/pvs/${VOLUME} 10.10.10.0/24(rw,sync,all_squash)" >> /etc/exports
+  mkdir -p /exports/nfs/pvs/${VOLUME}
+  echo "/dev/mapper/${VG}-lv_${VOLUME} /exports/nfs/pvs/${VOLUME} xfs defaults 0 0" >> /etc/fstab
+  echo "/exports/nfs/pvs/${VOLUME} 10.10.10.0/24(rw,sync,all_squash)" >> /etc/exports
 done
 mount -a
 # https://docs.openshift.com/enterprise/3.0/admin_guide/persistent_storage_nfs.html
 for VOLUME in pv{1..10}
 do
-  chown nfsnobody:nfsnobody /export/nfs/pvs/${VOLUME}
-  chmod 777  /export/nfs/pvs/${VOLUME}
+  chown nfsnobody:nfsnobody /exports/nfs/pvs/${VOLUME}
+  chmod 777  /exports/nfs/pvs/${VOLUME}
 done
 systemctl restart nfs
-exportfs -a
+exportsfs -a
 
 VG=rhel
 LVSIZE=1g
 VOLUME=registryvol
 lvcreate -L${LVSIZE} -nlv_${VOLUME} $VG
 mkfs.xfs -f /dev/mapper/${VG}-lv_${VOLUME}
-mkdir -p /export/nfs/pvs/${VOLUME}
-echo "/dev/mapper/${VG}-lv_${VOLUME} /export/nfs/pvs/${VOLUME} xfs defaults 0 0" >> /etc/fstab
-echo "/export/nfs/pvs/${VOLUME} *(rw,sync,all_squash)" >> /etc/exports
-mount /export/nfs/pvs/${VOLUME}
-chown nfsnobody:nfsnobody /export/nfs/pvs/${VOLUME}
-chmod 777  /export/nfs/pvs/${VOLUME}
+mkdir -p /exports/nfs/pvs/${VOLUME}
+echo "/dev/mapper/${VG}-lv_${VOLUME} /exports/nfs/pvs/${VOLUME} xfs defaults 0 0" >> /etc/fstab
+echo "/exports/nfs/pvs/${VOLUME} *(rw,sync,all_squash)" >> /etc/exports
+mount /exports/nfs/pvs/${VOLUME}
+chown nfsnobody:nfsnobody /exports/nfs/pvs/${VOLUME}
+chmod 777  /exports/nfs/pvs/${VOLUME}
 systemctl restart nfs
 
