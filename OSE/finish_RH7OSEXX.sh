2bd72eb387728ea3b12c947994db537a1c58f3b9
jradtke-rh <jradtke@redhat.com>
2016-05-12 23:42:22 -0400

    dynamic-update was missing boolean

--- OSE/finish_RH7OSEXX.sh
+++ OSE/finish_RH7OSEXX.sh
@@ -215,7 +215,7 @@ exit 0
 update_dns() {
 ssh rh7idm01
 kinit admin
-ipa dnszone-add cloudapps.aperture.lab --admin-email=root@aperture.lab --minimum=3000 --dynamic-update
+ipa dnszone-add cloudapps.aperture.lab --admin-email=root@aperture.lab --minimum=3000 --dynamic-update=true
 ipa dnsrecord-add cloudapps.aperture.lab '*' --a-rec 192.168.122.135
 ipa dnsrecord-add cloudapps.aperture.lab '*' --a-rec 192.168.122.136
 ipa dnszone-mod --allow-transfer='192.168.122.0;127.0.0.1' aperture.lab
